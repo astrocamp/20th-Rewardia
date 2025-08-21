49525f33d1b840fe90c6a6bf46d854aaff0c9c3a
samuel <legosthes@gmail.com>
2025-08-21 10:02:15 +0800

    fixed: 重新命名為card_crawler

--- apps/card_crawler/apps.py
+++ apps/card_crawler/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class OutputTextConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.output_text'
+    name = 'apps.card_crawler'
