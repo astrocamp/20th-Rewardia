e81a3b08bd09deea3477392c7a83cac6df1eff03
samuel <legosthes@gmail.com>
2025-08-21 22:15:53 +0800

    feat: 新增card_crawler模型及makemigrations

--- apps/card_crawler/apps.py
+++ apps/card_crawler/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CardCrawlerConfig(AppConfig):
-    default_auto_field = 'django.db.smodels.BigAutoField'
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.card_crawler'
