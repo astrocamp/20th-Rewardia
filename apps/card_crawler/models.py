9aca0796b8e4d392d2b963391dd64ebb176b8373
samuel <legosthes@gmail.com>
2025-08-21 22:15:53 +0800

    feat: 新增card_crawler模型及makemigrations

--- apps/card_crawler/models.py
+++ apps/card_crawler/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from apps.banks.models import Bank
+from apps.cards.models import CreditCard
 
 # Create your models here.
+class CrawledData(models.Model):
+    name = models.ForeignKey(
+        CreditCard,
+        on_delete=models.CASCADE,
+        related_name='crawled_data',
+        verbose_name='Credit Card'
+    )
+    bank = models.ForeignKey(
+        Bank,
+        on_delete=models.CASCADE,
+        related_name='credit_cards_crawled_data',
+        verbose_name='Issuing Bank'
+    )
+    content = models.TextField('Crawled Content',null=False)
+    url = models.CharField('Crawled Website',null=False)
+    created_at = models.DateTimeField('Created At', auto_now_add=True)
+
+    class Meta:
+        db_table = 'crawler_data'
+        verbose_name = 'Crawled Card'
+        verbose_name_plural = 'Crawled Cards'
+
+    def __str__(self):
+        return self.name
--- apps/nlp_validation/apps.py
+++ apps/nlp_validation/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class NlpValidationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'nlp_validation'
+    name = 'apps.nlp_validation'
