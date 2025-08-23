aaa98785d9a44633534f49f18400a2d9bc12d087
samuel <legosthes@gmail.com>
2025-08-23 22:34:01 +0800

    fixed: 新增滑動到按鈕再按及整個程式的try except

--- apps/card_crawler/roo.py
+++ apps/card_crawler/roo.py
@@ -10,112 +10,117 @@ import time
 
 driver = webdriver.Chrome()
 
-main_url_page = "https://roo.cash/creditcard/"
-sleep_time = (random.randint(7,14))/10
-
 try:
-    driver.get(main_url_page)
-    # 在尋找物件前要等多久
-    driver.implicitly_wait(2)
-except WebDriverException as error:
-    print(f"Error fetching main page: {error}")
-    driver.quit()
-
-
-try:
-    card_names = driver.find_elements(By.CSS_SELECTOR, "a[data-testid='product-detail']")
-    card_urls = [card.get_attribute('href') for card in card_names]
-except WebDriverException as error:
-    print(f"Error fetching element: {error}")
-
-def get_card_info(url):
+    main_url_page = "https://roo.cash/creditcard/"
+    sleep_time = random.uniform(7,14)
 
     try:
-        print(f"Processing: {url}")
-        driver.get(url)
+        driver.get(main_url_page)
+        # 在尋找物件前要等多久
+        driver.implicitly_wait(2)
+    except WebDriverException as error:
+        print(f"Error fetching main page: {error}")
+        driver.quit()
 
-        content_arr = []
 
-        try:
-            # 找到卡的名字
-            card_name = driver.find_element(By.CSS_SELECTOR,'h1[data-testid="product-title"]')
-            # 把卡片加到content_arr裡，卡片名是串列裡的第一個元素
-            # strip 會刪除前後的空格
-            # 把card和url存成字典，存成串列的第一個元素，方便查找資料
-            content_arr.append({
-                "card":f"{card_name.text.strip()}",
-                "url":f"{url}"
-            })
-        except WebDriverException as error:
-            # 找不到卡的新增一個找不到的資訊
-            error_data = {
-                "card":"Card not found.",
-                "url":f"{url}",
-                "error":f"{error}"
-            }
-            content_arr.append(error_data)
-            print(f"Error fetching card: {error}")
-            return error_data
+    try:
+        card_names = driver.find_elements(By.CSS_SELECTOR, "a[data-testid='product-detail']")
+        card_urls = [card.get_attribute('href') for card in card_names]
+    except WebDriverException as error:
+        print(f"Error fetching element: {error}")
 
+    def get_card_info(url):
 
         try:
-            # 找到navbar和footer之間的所有button
-            buttons = driver.find_elements(locate_with(By.TAG_NAME, "button").below({By.TAG_NAME: "nav"}).above({By.TAG_NAME:"footer"}))
-            action = ActionChains(driver)
+            print(f"Processing: {url}")
+            driver.get(url)
 
-            # 點選這些button，讓他們展開，因為資訊隱藏在裡面
-            for button in buttons:
-                action.click(on_element=button)
+            content_arr = []
 
-            # perform會執行click的動作
-            action.perform()
             try:
-                # 比較節省時間的等待方式，如果元素在5秒內還沒出現就會回傳錯誤
-                WebDriverWait(driver,5).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "answer")))
-            except TimeoutException:
-                print("Error fetching elements")
-        except WebDriverException as error:
-            print(f"Error fetching button: {error}")
+                # 找到卡的名字
+                card_name = driver.find_element(By.CSS_SELECTOR,'h1[data-testid="product-title"]')
+                # 把卡片加到content_arr裡，卡片名是串列裡的第一個元素
+                # strip 會刪除前後的空格
+                # 把card和url存成字典，存成串列的第一個元素，方便查找資料
+                content_arr.append({
+                    "card":f"{card_name.text.strip()}",
+                    "url":f"{url}"
+                })
+            except WebDriverException as error:
+                # 找不到卡的新增一個找不到的資訊
+                error_data = {
+                    "card":"Card not found.",
+                    "url":f"{url}",
+                    "error":f"{error}"
+                }
+                content_arr.append(error_data)
+                print(f"Error fetching card: {error}")
+                return error_data
 
-        # 找到navbar和footer之間的所有h4, h5, li, p的元素
+
+            try:
+                # 找到navbar和footer之間的所有button
+                buttons = driver.find_elements(locate_with(By.TAG_NAME, "button").below({By.TAG_NAME: "nav"}).above({By.TAG_NAME:"footer"}))
+                action = ActionChains(driver)
+
+                # 點選這些button，讓他們展開，因為資訊隱藏在裡面
+                for button in buttons:
+                    driver.execute_script("arguments[0].scrollIntoView(true);",button)
+                    action.move_to_element(button).click(on_element=button)
+
+                # perform會執行click的動作
+                action.perform()
+                try:
+                    # 比較節省時間的等待方式，如果元素在5秒內還沒出現就會回傳錯誤
+                    WebDriverWait(driver,5).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "answer")))
+                except TimeoutException:
+                    print("Error fetching elements")
+            except WebDriverException as error:
+                print(f"Error fetching button: {error}")
+
+            # 找到navbar和footer之間的所有h4, h5, li, p的元素
+            try:
+                contents = driver.find_elements(locate_with(By.CSS_SELECTOR,"h4,h5,li,p").above({By.XPATH:"//h2[text()='其他推薦信用卡']"}).below({By.TAG_NAME: "nav"}))
+                # 把它存到array
+                for content in contents:
+                    # 排出空白的
+                    if len(content.text)>1:
+                        content_arr.append(content.text)
+
+                # 成功的話，才會回傳值
+                return content_arr
+            except WebDriverException as error:
+                print(f"Error finding content: {error}")
+
+        except WebDriverException as error:
+            print(f"Card not found: {error}")
+            # 找不到卡，就終止這次的函式
+            return error
+
+    failed_cards = []
+    all_cards_data = []
+
+    for i, card_url in enumerate(card_urls):
         try:
-            contents = driver.find_elements(locate_with(By.CSS_SELECTOR,"h4,h5,li,p").above({By.XPATH:"//h2[text()='其他推薦信用卡']"}).below({By.TAG_NAME: "nav"}))
-            # 把它存到array
-            for content in contents:
-                # 排出空白的
-                if len(content.text)>1:
-                    content_arr.append(content.text)
-
-            # 成功的話，才會回傳值
-            return content_arr
+            # 找尋每張卡頁面的資料
+            card_data = get_card_info(card_url)
+            # 將資料存到這個串列
+            all_cards_data.append(card_data)
+            print(card_data)
+            print(f"Processing {card_data[0]['card']} finished")
+            time.sleep(sleep_time)
+    
         except WebDriverException as error:
-            print(f"Error finding content: {error}")
-
-    except WebDriverException as error:
-        print(f"Card not found: {error}")
-        # 找不到卡，就終止這次的函式
-        return error
-
-failed_cards = []
-all_cards_data = []
-
-for i, card_url in enumerate(card_urls):
-    try:
-        # 找尋每張卡頁面的資料
-        card_data = get_card_info(card_url)
-        # 將資料存到這個串列
-        all_cards_data.append(card_data)
-        print(card_data)
-        print(f"Processing {card_data[0]['card']} finished")
-        time.sleep(sleep_time)
-  
-    except WebDriverException as error:
-        failed_cards.append(card_url)
-        print(f"Error fetching page {card_url}: {error}")
+            failed_cards.append(card_url)
+            print(f"Error fetching page {card_url}: {error}")
 
 
-print(f"Successfully processed: {len(all_cards_data)} cards")
-print(f"Failed: {len(failed_cards)} cards")
+    print(f"Successfully processed: {len(all_cards_data)} cards")
+    print(f"Failed: {len(failed_cards)} cards")
 
-# 關閉查找
-driver.quit()
+
+finally:
+    # 關閉查找
+    print("Closing webdriver.")
+    driver.quit()
