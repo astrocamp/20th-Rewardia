0b1cb6eab230fbdac06d1407993304a394b7ed1e
朱怡潔 <yichieh@zhuyijiedeMacBook-Pro.local>
2025-08-24 20:04:07 +0800

    會員專區html切版#37

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from apps.users.views import member_zone
 
 
 app_name = "pages"
@@ -9,9 +10,5 @@ urlpatterns = [
     path('calculator/', views.calculator, name="calculator"),
     path('card_new/', views.card_new, name="card_new"),
     path('register/', views.register, name="register"),
-
-
-] + debug_toolbar_urls()
-
-    
-
+    path('member/', member_zone, name='member_zone'),
+]
