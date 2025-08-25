bb722f6b127e46fcab97fbe189eb75e649b97421
samuel <legosthes@gmail.com>
2025-08-25 23:53:35 +0800

    feat: 新增url domain的db欄位

--- apps/card_crawler/models.py
+++ apps/card_crawler/models.py
@@ -10,8 +10,8 @@ class CrawledData(models.Model):
         verbose_name='Credit Card'
     )
     content = models.TextField('Crawled Content',null=False)
-    url = models.URLField('Crawled Website',null=False)
-    url_domain = models.URLField('Crawled Main Source', default=)
+    url = models.URLField('Crawled Website',default=None)
+    url_domain = models.URLField('Crawled Main Source', default=None)
     created_at = models.DateTimeField('Created At', auto_now_add=True)
 
     class Meta:
