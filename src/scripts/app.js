724291a30a78a3f025724cc323e1c77ce358dad2
Brad Yu <samwalila97@gmail.com>
2025-08-25 22:05:48 +0800

    feat: 在login與register頁面引入htmx處理註冊與登入提交

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,55 +5,48 @@ window.Alpine = Alpine;
 
 // FAQ 頁面的 Alpine.js 功能
 Alpine.data('faqAccordion', () => ({
-    openItem: null,
-    
-    toggleItem(itemId) {
-        if (this.openItem === itemId) {
-            this.openItem = null;
-        } else {
-            this.openItem = itemId;
-        }
-    },
-    
-    isOpen(itemId) {
-        return this.openItem === itemId;
+  openItem: null,
+  
+  toggleItem(itemId) {
+    if (this.openItem === itemId) {
+      this.openItem = null;
+    } else {
+      this.openItem = itemId;
     }
+  },
+  
+  isOpen(itemId) {
+    return this.openItem === itemId;
+  }
 }));
 
 // 註冊頁面的 Alpine.js 功能
 Alpine.data('registerForm', () => ({
-    // 密碼顯示狀態追蹤
-    passwordVisible: false,
-    confirmPasswordVisible: false,
-    
-    // 切換密碼顯示/隱藏
-    togglePassword(fieldType) {
-        if (fieldType === 'password') {
-            this.passwordVisible = !this.passwordVisible;
-        } else if (fieldType === 'confirm') {
-            this.confirmPasswordVisible = !this.confirmPasswordVisible;
-        }
-    },
-    
-    // 表單提交處理
-    submitForm() {
-        // 目前只是先預留框架, 後續預期功能擴展(例如驗證或數據收集處理)
-        console.log('表單提交處理');
+  // 密碼顯示狀態追蹤
+  passwordVisible: false,
+  confirmPasswordVisible: false,
+  
+  // 切換密碼顯示/隱藏
+  togglePassword(fieldType) {
+    if (fieldType === 'password') {
+      this.passwordVisible = !this.passwordVisible;
+    } else if (fieldType === 'password_confirm') {
+      this.confirmPasswordVisible = !this.confirmPasswordVisible;
     }
+  },
+  
+  // 表單提交由 HTMX 處理，此函數已不需要
 }));
 
 // 登入頁面的 Alpine.js 功能
 Alpine.data('loginForm', () => ({
-    passwordVisible: false,
-    
-    togglePassword() {
-        this.passwordVisible = !this.passwordVisible;
-    },
-    
-    submitForm() {
-        // 登入表單提交邏輯
-        console.log('登入表單提交');
-    }
+  passwordVisible: false,
+  
+  togglePassword() {
+    this.passwordVisible = !this.passwordVisible;
+  },
+  
+  // 表單提交由 HTMX 處理，此函數已不需要
 }));
 
 Alpine.start();
