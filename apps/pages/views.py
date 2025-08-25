a7f77be0319ba7ea8493c83e1c2dae2d398904a2
Brad Yu <samwalila97@gmail.com>
2025-08-25 22:21:40 +0800

    feat: 將faq.html檔中的問與答文字抽出成立pages/data/faq_content.py檔再引入faq.html檔

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .data.faq_content import FAQ_DATA
 
 def home(request):
   return render(request, "pages/home.html")
@@ -10,7 +11,10 @@ def calculator(request):
   return render(request, "pages/calculator.html")
 
 def faq(request):
-  return render(request, "pages/faq.html")
+  context = {
+    'faq_categories': FAQ_DATA['categories']
+  }
+  return render(request, "pages/faq.html", context)
 
 def register(request):
   return render(request, "pages/register.html")
