d424c711926f473028c771d51291e5d71642b5cd
samuel <legosthes@gmail.com>
2025-08-26 17:31:23 +0800

    feat: crawled_data模型updated_at欄位

--- apps/card_crawler/models.py
+++ apps/card_crawler/models.py
@@ -1,23 +1,25 @@
 from django.db import models
 from apps.cards.models import CreditCard
 
+
 # Create your models here.
 class CrawledData(models.Model):
     card = models.ForeignKey(
         CreditCard,
         on_delete=models.CASCADE,
-        related_name='crawled_data',
-        verbose_name='Credit Card'
+        related_name="crawled_data",
+        verbose_name="Credit Card",
     )
-    content = models.TextField('Crawled Content',null=False)
-    url = models.URLField('Crawled Website',default=None)
-    url_domain = models.URLField('Crawled Main Source', default=None)
-    created_at = models.DateTimeField('Created At', auto_now_add=True)
+    content = models.TextField("Crawled Content", null=False)
+    url = models.URLField("Crawled Website", default=None)
+    url_domain = models.URLField("Crawled Main Source", default=None)
+    created_at = models.DateTimeField("Created At", auto_now_add=True)
+    updated_at = models.DateTimeField("Updated At", auto_now=True)
 
     class Meta:
-        db_table = 'crawler_data'
-        verbose_name = 'Crawled Card'
-        verbose_name_plural = 'Crawled Cards'
+        db_table = "crawler_data"
+        verbose_name = "Crawled Card"
+        verbose_name_plural = "Crawled Cards"
 
     def __str__(self):
         return self.card.name
