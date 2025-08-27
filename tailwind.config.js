a4278be53090f71cf8e919f97f6ef7e5a5240c58
Brad Yu <samwalila97@gmail.com>
2025-08-27 13:10:42 +0800

    fixed: 再次檢查css中是否仍有未轉換成tailwind的樣式並轉換

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,17 @@ module.exports = {
       colors: {
         'primary': '#2060B9',
         'primary-dark': '#193966',
+        'primary-hover': '#1c55a4',
+        'primary-active': '#184a90',
+        'primary-disabled': '#9BB6DD',
         'muted': '#5D6970',
         'light-gray': '#F9F9F9',
+        'dark-gray': '#111827',
+        'border-light': '#D1D5DB',
+        'placeholder-gray': '#949AA6',
+        'error-red': '#FF1F1F',
+        'bg-light-alt': '#E5E7EB',
+        'border-subtle': '#BEC1C6',
       },
       fontFamily: {
         'kulim-park': ['"Kulim Park"', 'sans-serif'],
