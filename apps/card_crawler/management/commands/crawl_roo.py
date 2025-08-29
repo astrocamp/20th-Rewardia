75f780a982a4e7a3ae63eb0b928ac536a3244b83
samuel <legosthes@gmail.com>
2025-08-29 10:36:12 +0800

    modify: 暫時先刪除再次處理failed cards的函式

--- apps/card_crawler/management/commands/crawl_roo.py
+++ apps/card_crawler/management/commands/crawl_roo.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from apps.card_crawler.roo import crawl_roo_cards, crawl_roo_urls, get_card_info
+from apps.card_crawler.roo import crawl_roo_cards, crawl_roo_urls
 import random
 
 
@@ -22,13 +22,7 @@ class Command(BaseCommand):
 
             self.stdout.write("🦘開始抓取分類中的信用卡資料🦘")
             crawl_roo_cards(category_urls)
-            self.stdout.write("✅抓取和存取資料完畢")
-        except Exception as error:
-            self.stdout.write(f"失敗: {error}")
 
-        try:
-            # 把失敗的卡再帶進來抓一次
-            for failed in self.failed_cards:
-                get_card_info(failed["url"])
+            self.stdout.write("✅抓取和存取資料完畢")
         except Exception as error:
             self.stdout.write(f"失敗: {error}")
