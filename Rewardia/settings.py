b4823227b56cdcbe4404bc3f927d94b4afa10a4c
samuel <legosthes@gmail.com>
2025-08-29 15:06:45 +0800

    feat: 創建admins應用程式做後台管理

--- Rewardia/settings.py
+++ Rewardia/settings.py
@@ -53,7 +53,11 @@ INSTALLED_APPS = [
     "apps.cards",
     "apps.merchants",
     "apps.rewards",
+<<<<<<< HEAD
     "apps.sessions",
+=======
+    "apps.admins",
+>>>>>>> 34d5176 (feat: 創建admins應用程式做後台管理)
     # ------------------------------
     "apps.nlp_validation",
     "apps.card_crawler",
@@ -156,9 +160,17 @@ INTERNAL_IPS = [
 
 
 MESSAGE_TAGS = {
+<<<<<<< HEAD
     messages.DEBUG: "tw-toast tw-info",
     messages.INFO: "tw-toast tw-info",
     messages.SUCCESS: "tw-toast tw-success",
     messages.WARNING: "tw-toast tw-warn",
     messages.ERROR: "tw-toast tw-error",
 }
+=======
+    messages.SUCCESS: "alert-success",
+    messages.ERROR: "alert-error",
+}
+
+ADMIN_ENABLED = False
+>>>>>>> 34d5176 (feat: 創建admins應用程式做後台管理)
