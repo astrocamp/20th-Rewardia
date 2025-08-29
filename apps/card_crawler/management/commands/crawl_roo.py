3e34e581be7705317365c35db7551e5b57810212
samuel <legosthes@gmail.com>
2025-08-29 11:41:49 +0800

    modify: 刪掉不必要的init函式

--- apps/card_crawler/management/commands/crawl_roo.py
+++ apps/card_crawler/management/commands/crawl_roo.py
@@ -6,13 +6,6 @@ import random
 class Command(BaseCommand):
     help = "在袋鼠金融抓取信用卡資料，並把銀行、信用卡名分別存進對應的資料庫，並把抓取到的資料存進抓取資料的資料庫。"
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.failed_cards = []
-        self.processed_urls = set()
-        # 仿效真人操作瀏覽器，隨機從0.7-1.4秒之間，挑選暫停秒數
-        self.sleep_time = random.uniform(7, 14)
-
     def handle(self, *args, **options):
         self.stdout.write("🦘開始抓取袋鼠金融信用卡🦘")
 
