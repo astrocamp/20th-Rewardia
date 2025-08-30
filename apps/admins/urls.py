b1742d50274f15bf21f8330cdab46050a88d3efc
samuel <legosthes@gmail.com>
2025-08-30 16:41:08 +0800

    feat: 新增刪除卡片功能

--- apps/admins/urls.py
+++ apps/admins/urls.py
@@ -6,5 +6,6 @@ app_name = "admins"
 urlpatterns = [
     path("cards/", views.cards, name="cards"),
     path("new_card/", views.new_card, name="new_card"),
+    path("delete_card/<int:id>/", views.delete_card, name="delete_card"),
     path("rewards/", views.rewards, name="rewards"),
 ]
