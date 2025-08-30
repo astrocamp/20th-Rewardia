53e4685a302ea8a6fad8dd31c1a372f1ed3345d9
samuel <legosthes@gmail.com>
2025-08-30 09:58:57 +0800

    feat: 新增銀行、發卡機構和卡片等級的選項

--- apps/admins/views.py
+++ apps/admins/views.py
@@ -1,13 +1,38 @@
 from django.shortcuts import render
 from apps.cards.models import CreditCard
+from apps.cards.models import Bank
 
 
 # Create your views here.
 def cards(request):
+    banks = Bank.objects.filter(is_active=True)
+    card_networks = CreditCard.CardNetwork
+    card_types = CreditCard.CardType
+
     if request.POST:
-        form = CreditCard.objects
-        pass
-    return render(request, "admins/cards.html")
+        name = request.POST.get("card")
+        bank = request.POST.get("bank")
+        card_network = request.POST.get("network")
+        card_type = request.POST.get("card_type")
+        foreign_transaction_fee = request.POST.get("foreign")
+        is_active = request.POST.get("is_active")
+        CreditCard.objects.create(
+            name=name,
+            bank=bank,
+            network=card_network,
+            card_type=card_type,
+            foreign_transaction_fee=foreign_transaction_fee,
+            is_active=is_active,
+        )
+    return render(
+        request,
+        "admins/cards.html",
+        {
+            "banks": banks,
+            "card_networks": card_networks,
+            "card_types": card_types,
+        },
+    )
 
 
 def rewards(request):
