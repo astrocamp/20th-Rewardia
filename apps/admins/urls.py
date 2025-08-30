012ff339b5938019a54aa0d44248ac103adb17f4
samuel <legosthes@gmail.com>
2025-08-30 11:27:07 +0800

    feat: 增加新增卡片存取至資料庫

--- apps/admins/urls.py
+++ apps/admins/urls.py
@@ -5,5 +5,6 @@ app_name = "admins"
 
 urlpatterns = [
     path("cards/", views.cards, name="cards"),
+    path("new_card/", views.new_card, name="new_card"),
     path("rewards/", views.rewards, name="rewards"),
 ]
