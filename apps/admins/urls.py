11b79d365a066c6af63a7cfd744edc3d48e0695e
samuel <legosthes@gmail.com>
2025-09-01 00:50:57 +0800

    feat:新增編輯卡片功能

--- apps/admins/urls.py
+++ apps/admins/urls.py
@@ -6,6 +6,8 @@ app_name = "admins"
 urlpatterns = [
     path("cards/", views.cards, name="cards"),
     path("new_card/", views.new_card, name="new_card"),
+    path("edit_card/<int:id>/", views.edit_card, name="edit_card"),
+    path("update_card/<int:id>/", views.update_card, name="update_card"),
     path("delete_card/<int:id>/", views.delete_card, name="delete_card"),
     path("rewards/", views.rewards, name="rewards"),
 ]
