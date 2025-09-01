555002b3e764f20df0366a4c21d2a0a012e9a802
samuel <legosthes@gmail.com>
2025-09-01 16:32:12 +0800

    modify: 改片新增和編輯卡片樣式

--- apps/admins/views.py
+++ apps/admins/views.py
@@ -11,43 +11,47 @@ from django.http import HttpResponse
 
 # 卡片頁面的函式
 def cards(request):
-    banks = Bank.objects.filter(is_active=True)
-    card_networks = CreditCard.CardNetwork
-    card_types = CreditCard.CardType
     cards = CreditCard.objects.order_by("-updated_at")
     return render(
         request,
         "admins/cards.html",
         {
-            "banks": banks,
-            "card_networks": card_networks,
-            "card_types": card_types,
             "cards": cards,
         },
     )
 
 
-@require_POST
 def new_card(request):
-    name = request.POST.get("card")
-    bank = request.POST["bank"]
-    card_network = request.POST["network"]
-    card_type = request.POST["card_type"]
+    banks = Bank.objects.filter(is_active=True)
+    card_networks = CreditCard.CardNetwork
+    card_types = CreditCard.CardType
+    card_form_data = {
+        "banks": banks,
+        "card_networks": card_networks,
+        "card_types": card_types,
+    }
 
-    is_active = request.POST.get("is_active") == "on"
-    new_card = CreditCard.objects.create(
-        name=f"{Bank.objects.get(id=bank).name} {name}",
-        bank=Bank.objects.get(id=bank),
-        card_network=card_network,
-        card_type=card_type,
-        is_active=is_active,
-    )
-    if new_card:
-        messages.success(request, "新增卡片成功")
-        return render(request, "admins/card_row.html", {"card": new_card})
+    if request.method == "POST":
+        name = request.POST.get("card")
+        bank = request.POST["bank"]
+        card_network = request.POST["network"]
+        card_type = request.POST["card_type"]
+        is_active = request.POST.get("is_active") == "on"
+        new_card = CreditCard.objects.create(
+            name=f"{Bank.objects.get(id=bank).name} {name}",
+            bank=Bank.objects.get(id=bank),
+            card_network=card_network,
+            card_type=card_type,
+            is_active=is_active,
+        )
+        if new_card:
+            messages.success(request, "新增卡片成功")
+            return render(request, "admins/new_card.html", {"card": new_card})
+        else:
+            messages.success(request, "新增失敗")
+            return redirect("admins:cards")
     else:
-        messages.success(request, "新增失敗")
-        return redirect("admins:cards")
+        return render(request, "admins/new_card.html", card_form_data)
 
 
 @require_http_methods(["GET"])
@@ -83,7 +87,7 @@ def update_card(request, id):
     card.is_active = request.POST.get("is_active_edit") == "on"
     card.save()
     messages.success(request, "更新成功")
-    return redirect("admins:all_cards")
+    return redirect("admins:cards")
 
 
 @require_http_methods(["POST"])
