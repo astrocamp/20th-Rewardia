967184a4feee483e539b7fe3f6e10e2ec95174f5
samuel <legosthes@gmail.com>
2025-09-02 10:37:26 +0800

    fixed: 解衝突並把messages新增到admins的default.html裡

--- Rewardia/urls.py
+++ Rewardia/urls.py
@@ -20,15 +20,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('admin/', admin.site.urls),
-    path('', include("apps.pages.urls")),
-    path('users/', include('apps.users.urls')),
-    path('sessions/', include('apps.sessions.urls')),
-=======
     # path("admin/", admin.site.urls),
     path("", include("apps.pages.urls")),
     path("users/", include("apps.users.urls")),
     path("admins/", include("apps.admins.urls")),
->>>>>>> 34d5176 (feat: 創建admins應用程式做後台管理)
 ] + debug_toolbar_urls()
