ac070c88e720f45695e350f84d87362de81bfc2c
Brad Yu <samwalila97@gmail.com>
2025-09-04 05:15:01 +0800

    feat: 建立相關html/css/js檔

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,12 @@
 import Alpine from "alpinejs";
 import "htmx.org";
+import message from "./components/message.js";
+import cardForm from "./components/card_form.js";
+import register from "./components/register.js";
+import faq from "./components/faq.js";
+import login from "./components/login.js";
+import subscribe from "./components/subscribe.js";
+import chatbot from "./components/chatbot.js";
 
 window.Alpine = Alpine;
 
@@ -9,5 +16,6 @@ Alpine.data("register_form", register);
 Alpine.data("faq_accordion", faq);
 Alpine.data("login_form", login);
 Alpine.data("subscribe_form", subscribe);
+Alpine.data("chatbot", chatbot);
 
 Alpine.start();
