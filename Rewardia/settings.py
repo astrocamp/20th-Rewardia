23018d3fc98bf5ea47339cf6e25880d6c64834bf
YSEthan <yushen1997@gmail.com>
2025-09-03 15:49:11 +0800

    feat:celery基本設定

--- Rewardia/settings.py
+++ Rewardia/settings.py
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
     # ------------------------------
     "django_celery_results",
     # ------------------------------
+    "django_celery_results",
+    # "kombu.transport.django"
+    # ------------------------------
     "debug_toolbar",
     # ------------------------------
     "apps.pages",
