e94fab0957e61caedbf2f69711537d8e4f8819d3
Yichieh <yichieh10002@gmail.com>
2025-09-05 05:19:57 +0800

    fixed:優惠試算功能開發+回饋規則管理後台 #82

--- apps/sessions/urls.py
+++ apps/sessions/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "sessions"
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register_view, name="register"),
 ]
