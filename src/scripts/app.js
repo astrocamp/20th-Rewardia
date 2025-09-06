49655104742bbbb125789cb6373a11b39c6c0e93
YSEthan <yushen1997@gmail.com>
2025-09-07 05:03:09 +0800

    fixed:修正會員系統新增卡片功能

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,7 +2,7 @@ import Alpine from "alpinejs";
 import "htmx.org";
 
 // Import all components
-import './components/header.js';
+import "./components/header.js";
 import chatbot from "./components/chatbot.js";
 import mainSearchComponent from './components/main.js';
 import cardFormComponent from './components/card_form.js';
@@ -15,12 +15,12 @@ import imageUploadComponent from './components/image_upload.js';
 
 window.Alpine = Alpine;
 
-Alpine.data('main_search', mainSearchComponent);
-Alpine.data('card_form', cardFormComponent);
-Alpine.data('faq_accordion', faqComponent);
-Alpine.data('login_form', loginComponent);
-Alpine.data('register_form', registerComponent);
-Alpine.data('subscribe_form', subscribeComponent);
+Alpine.data("main_search", mainSearchComponent);
+Alpine.data("card_form", cardFormComponent);
+Alpine.data("faq_accordion", faqComponent);
+Alpine.data("login_form", loginComponent);
+Alpine.data("register_form", registerComponent);
+Alpine.data("subscribe_form", subscribeComponent);
 Alpine.data("toast_fadeout", messageComponent);
 Alpine.data("chatbot", chatbot);
 Alpine.data("imageUpload", imageUploadComponent);
