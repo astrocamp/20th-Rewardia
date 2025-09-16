6005b8719324439aa471251ce63a11e45194793e
samuel <legosthes@gmail.com>
2025-09-16 22:03:27 +0800

    feat: 創建隱私權頁面

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "pages"
 urlpatterns = [
     path("download/", views.download, name="download"),
+    path("privacy/", views.privacy, name="privacy"),
     path("", views.main, name="main"),
     path("faq/", views.faq, name="faq"),
     path("calculator/", views.calculator, name="calculator"),
@@ -22,5 +23,9 @@ urlpatterns = [
     path("get-messages/", views.get_messages, name="get_messages"),
     path("calculate-reward/", views.calculate_reward, name="calculate_reward"),
     path("api/main-data/", views.get_main_data, name="get_main_data"),
-    path("api/merchants-by-category/", views.get_merchants_by_category, name="get_merchants_by_category"),
+    path(
+        "api/merchants-by-category/",
+        views.get_merchants_by_category,
+        name="get_merchants_by_category",
+    ),
 ]
